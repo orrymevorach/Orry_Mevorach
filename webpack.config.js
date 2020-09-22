47c4008c1d1891dd476ad3d60ca21815b0980407
Orry Mevorach <orrymevorach@Orrys-MacBook-Pro.local>
2020-09-21 22:58:51 -0400

    add webpack, remove gulp; move assets to s3 bucket

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: ["./src/scripts/app.js", "./src/styles/styles.scss"],
+  devServer: {
+    contentBase: path.join(__dirname, "./"),
+    port: 9000,
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "file-loader",
+            options: { name: "styles.css" },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
