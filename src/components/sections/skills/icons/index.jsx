10a01f556c511ae59c53bd4a1a52476606070255
Orry Mevorach <orry.mevorach@gmail.com>
2021-01-06 00:35:01 -0500

    re-order skills

---
+++ src/components/sections/skills/icons/index.jsx
@@ -0,0 +1,33 @@
+import AirtableIcon from './airtable.jsx';
+import ApolloIcon from './apollo.jsx';
+import ContentfulIcon from './contentful.jsx';
+import FirebaseIcon from './firebase.jsx';
+import GitIcon from './git.jsx';
+import GraphQLIcon from './graphql.jsx';
+import JavascriptIcon from './javascript.jsx';
+import JestIcon from './jest.jsx';
+import NetlifyIcon from './netlify.jsx';
+import NextIcon from './next.jsx';
+import PuppeteerIcon from './puppeteer.jsx';
+import ReactIcon from './react.jsx';
+import ReactRouterIcon from './reactRouter.jsx';
+import ReduxIcon from './redux.jsx';
+import StorybookIcon from './storybook.jsx';
+
+export {
+  AirtableIcon,
+  ApolloIcon,
+  ContentfulIcon,
+  FirebaseIcon,
+  GitIcon,
+  GraphQLIcon,
+  JavascriptIcon,
+  JestIcon,
+  NetlifyIcon,
+  NextIcon,
+  PuppeteerIcon,
+  ReactIcon,
+  ReactRouterIcon,
+  ReduxIcon,
+  StorybookIcon,
+};
