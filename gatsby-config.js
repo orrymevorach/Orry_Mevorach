991c8cbd3c12d9721973af67fdad4d8aa89c8bf6
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2022-10-07 11:58:50 -0400

    remove unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-sass',
-    'gatsby-plugin-image',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
     {
       resolve: `gatsby-plugin-alias-imports`,
       options: {
