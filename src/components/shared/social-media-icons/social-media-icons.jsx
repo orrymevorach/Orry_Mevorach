64414964278981962cfe2267c228b0ef10879f85
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-04-24 22:10:30 -0700

    add instagram icon

--- src/components/shared/social-media-icons/social-media-icons.jsx
+++ src/components/shared/social-media-icons/social-media-icons.jsx
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react';
 import './social-media-icons.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import {
+  faGithub,
+  faLinkedinIn,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { ScreenWidthContext } from '@root/context/screenWidthContext';
 
@@ -19,6 +23,14 @@ export default function SocialMediaIcons() {
         >
           <FontAwesomeIcon icon={faGithub} size={iconSize} />
         </a>
+      </div>
+
+      <div className="instagram-container">
+        <div className="icon-circle">
+          <a href="https://www.instagram.com/mev.dev.o/" className="email-icon">
+            <FontAwesomeIcon icon={faInstagram} size={iconSize} />
+          </a>
+        </div>
       </div>
 
       <div className="linked-in-container">
